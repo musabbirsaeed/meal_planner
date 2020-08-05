87cdcc61c6c719301eebbbdaa94590f1662adf0e
musabbirsaeed <m.a.saeed@gmail.com>
2020-08-05 03:05:16 -0400

    make it better

--- hello.py
+++ hello.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return '<h1>Hello Yahoo! is an American web services provider headquartered in Sunnyvale, California, and owned by Verizon Media. The original Yahoo! company was founded by Jerry Yang and David Filo in January 1994 and was incorporated on March 2, 1995. Yahoo was one of the pioneers of the early Internet era in the 1990s  World!</h1>'
